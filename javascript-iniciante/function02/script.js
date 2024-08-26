7d0aa8a025aa14fa7b93f5ea0f237b3665b73971
Felipe Lemos <139652554+flplemos@users.noreply.github.com>
2024-08-25 22:26:29 -0300

    function 02

--- javascript-iniciante/function02/script.js
+++ javascript-iniciante/function02/script.js
@@ -93,3 +93,73 @@ const filtro = Array.prototype.filter.call(li, function(item) {
 });
 
 filtro; // Retrona os itens que possuem ativo. 
+
+//function.apply()
+//O apply(this, [arg1, arg2, ...]) Funciona como o call, a única diferença é que os argumentos da função são passados através de uma array.
+const numeros = [3,4,6,1,34,44,32];
+Math.max.apply(null, numeros); // 44
+Math.max.call(null, 3, 4, 5, 6, 7, 20);
+//Podemos passar o null para o valor de this, caso a função não utilize o objeto principal para funcionar.
+
+//Apply vs Call
+//A única dierença é a array como segundo argumento. 
+const li1 = document.querySelectorAll('li');
+
+function itemPossuiAtivo(item){
+    return item.classList.contains('ativo')
+}
+
+const filtro1 = Array.prototype.filter.call(li, itemPossuiAtivo);
+const filtro2 = Array.prototype.filter.apply(li, [itemPossuiAtivo]);
+
+//function.bind()
+//Diferente de call e apply, bind(this, arg1, arg2, ...) não irá executar a função mas sim retornar a mesma com o novo contexto de this. 
+const li2 = document.querySelectorAll('li');
+const filtrarLi = Array.prototype.filter.bind(li, function(item){
+    return item.classList.contains('ativo');
+})
+
+filtrarLi();
+
+//Argumentos e Bind
+//Não precisamos passar todos os argumentos no momento do bind, podemos passar os mesmos na nova função no momento da execução da mesma. 
+const carro1 = {
+    marca: 'Ford',
+    ano: 2018,
+    acelerar: function(aceleracao, tempo){
+        return `${this.marca} acelerou ${aceleracao} em ${tempo}`;
+    }
+}
+carro1.acelerar(100,20); //"Ford acelerou 100 em 20"
+
+const honda = {
+    marca: 'Honda'
+};
+
+const acelerarHonda = carro1.acelerar.bind(honda);
+acelerarHonda(200, 10); //"Honda acelerou 200 em 10"
+
+//Argumentos Comuns
+//Podemos passar argumentos padrões para uma função e retornar uma nova função.
+function imc(altura, peso){
+    return peso / (altura * altura);
+}
+
+const imc180 = imc.bind(null, 1.80);
+imc(1.80, 70); // 21.61
+imc180(70); // 21.61
+
+//Exercícios!!!
+
+//Crie uma função que retorne novos elementos HTML, com os seguintes parâmetros: tag, classe e conteudo. 
+
+function novoElementoHTML(tag, classe, conteudo){
+    const newElement = document.createElement(tag);
+     conteudo ? newElement.innerHTML = conteudo : ''; 
+     classe ? newElement.classList.add(classe) : '';
+     return newElement;
+}
+
+//Crie uma nova função utilizando a anterior como base, essa nova função deverá sempre criar h1 com a classe titulo. Porém o parâmetro conteudo continuará dinamico. 
+
+const h1Titulo = novoElementoHTML.bind(null, 'h1', 'titulo');
